80c04160a3fcf2b6d9dc5478e361d25f72d1a3a9
kashiwara0205 <tamatebako0205@gmail.com>
2022-04-15 17:26:39 +0900

    Init tutorial project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 柏原風希 on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
