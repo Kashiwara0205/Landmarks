61beb37dd661c50d18f8ef282f25e5836c8305ab
Kashiwara0205 <tamatebako0205@gmail.com>
2022-04-16 12:27:26 +0900

    Add CircleImage view

---
+++ Landmarks/CircleImag.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImag.swift
+//  Landmarks
+//
+//  Created by 柏原風希 on 2022/04/16.
+//
+
+import SwiftUI
+
+struct CircleImag: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImag_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImag()
+    }
+}
