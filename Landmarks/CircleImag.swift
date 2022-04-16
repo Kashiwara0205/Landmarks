0c93a63b72e295100bf829f3b9886aa0f68491fd
Kashiwara0205 <tamatebako0205@gmail.com>
2022-04-16 12:34:38 +0900

    Add circle image

--- Landmarks/CircleImag.swift
+++ Landmarks/CircleImag.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct CircleImag: View {
     var body: some View {
         Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.gray, lineWidth: 4)
+            )
+            .shadow(radius: 7)
     }
 }
 
