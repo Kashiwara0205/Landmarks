d33abd2a4860f25d1ad46c70fecb9ea4a6a4f570
Kashiwara0205 <tamatebako0205@gmail.com>
2022-04-24 15:37:52 +0900

    landmarkRowで写真が表示されるようにした

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -11,7 +11,14 @@ struct LandmarkRow: View {
     var landmark: Landmark
 
     var body: some View {
-        Text(landmark.name)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+                Text(landmark.name)
+                
+            Spacer()
+        }
     }
 }
 
